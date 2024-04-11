c3807e9787c4d9f51dc5a80f0ffe91e034521e53
VCGithubCode <vernellclark@hotmail.com>
2024-04-11 11:08:51 +0200

    Add sweetalert2 library and message

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,26 @@ function displayWorkingMessage() {
 }
 
 displayWorkingMessage();
+
+document.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
+  swalWithBootstrapButtons.fire({
+    title: 'Interested in the source code?',
+    text: 'Instead of inspecting my code this way... How about you fork my repo and give it a star instead?',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Yes, take me there!',
+    cancelButtonText: 'No, thank you.',
+    reverseButtons: true
+  }).then((result) => {
+    if (result.isConfirmed) {
+      window.location.href = 'https://github.com/VCGithubCode/vernell-c-new-repo-generator';
+    } else if (result.dismiss === Swal.DismissReason.cancel) {
+      swalWithBootstrapButtons.fire(
+        'Cancelled',
+        'No worries, feel free to continue exploring!',
+        'error'
+      );
+    }
+  });
+});
